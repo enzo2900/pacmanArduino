52622d0aa1b7c232d96a769dafc58dd7f0f40968
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-03-26 11:08:50 +0100

    correction bug

--- Vecteur2D.h
+++ Vecteur2D.h
@@ -18,6 +18,9 @@ struct Vecteur2D
   Vecteur2D copy() {
     return {x,y};
   }
+  Vecteur2D copy() {
+    return {x,y};
+  }
   Vecteur2D operator+(const Vecteur2D & velocity) const {
         return {x + velocity.x, y + velocity.y};
     }
