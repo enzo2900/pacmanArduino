7eeb6cd33cd34df430f36339716046531f483ec3
Mateofsr <mateo.faussurier@iut-rodez.fr>
2026-04-02 12:34:37 +0200

    fix delay

--- commande/commande.ino
+++ commande/commande.ino
@@ -7,13 +7,13 @@ int pinBtnGauche = 2;
 int pinBtnDroit = 3;
 int pinBtnHaut = 18;
 int pinBtnBas = 19;
-
+volatile unsigned long dela = 10000L;
+volatile unsigned long lastTime;
 void setup() {
   // Communication
-  Serial1.begin(9600);
-
-  
-  // Gestion des boutons
+  lastTime = millis();
+  Serial2.begin(9600);
+  Serial.begin(9600);
   pinMode(pinBtnGauche,INPUT_PULLUP);
   pinMode(pinBtnDroit,INPUT_PULLUP);
   pinMode(pinBtnHaut,INPUT_PULLUP);
@@ -25,23 +25,42 @@ void setup() {
 
 }
 
-
+bool delayBouton() {
+  long current = millis();
+  if(current > lastTime +dela) {
+    lastTime = current;
+    return true;
+  }
+  return false;
+}
 void gauche() {
-    Serial1.println("g");
+  if(!delayBouton) return; 
+  Serial2.println('g');
+   Serial.println('g');
+
 }
 
 void droite() {
-  Serial1.println("d");
+  if(!delayBouton) return; 
+  Serial2.println('d');
+  Serial.println('d');
+
 }
 
 void haut() {
-  Serial1.println("h");
+  if(!delayBouton) return; 
+  Serial2.println('h');
+  Serial.println('h');
+
 }
 
 void bas() {
-  Serial1.println("b");
+  if(!delayBouton) return; 
+  Serial2.println('b');
+  Serial.println('b');
 }
 
 void loop() {
-  
+  //delay(1000);
+  //Serial2.println('g');
 }
