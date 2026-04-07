048c3f528f16cb3dcfa755dd59ce44bc9c83f487
Mateofsr <mateo.faussurier@iut-rodez.fr>
2026-04-07 14:19:17 +0200

    chore: Simply the code

--- commande/commande.ino
+++ commande/commande.ino
@@ -7,13 +7,10 @@ int pinBtnGauche = 2;
 int pinBtnDroit = 3;
 int pinBtnHaut = 18;
 int pinBtnBas = 19;
-volatile unsigned long dela = 10000L;
-volatile unsigned long lastTime;
+
 void setup() {
   // Communication
-  lastTime = millis();
   Serial2.begin(9600);
-  Serial.begin(9600);
   pinMode(pinBtnGauche,INPUT_PULLUP);
   pinMode(pinBtnDroit,INPUT_PULLUP);
   pinMode(pinBtnHaut,INPUT_PULLUP);
@@ -25,42 +22,22 @@ void setup() {
 
 }
 
-bool delayBouton() {
-  long current = millis();
-  if(current > lastTime +dela) {
-    lastTime = current;
-    return true;
-  }
-  return false;
-}
 void gauche() {
-  if(!delayBouton) return; 
   Serial2.println('g');
-   Serial.println('g');
-
 }
 
 void droite() {
-  if(!delayBouton) return; 
   Serial2.println('d');
-  Serial.println('d');
-
 }
 
 void haut() {
-  if(!delayBouton) return; 
   Serial2.println('h');
-  Serial.println('h');
 
 }
 
 void bas() {
-  if(!delayBouton) return; 
   Serial2.println('b');
-  Serial.println('b');
 }
 
 void loop() {
-  //delay(1000);
-  //Serial2.println('g');
 }
